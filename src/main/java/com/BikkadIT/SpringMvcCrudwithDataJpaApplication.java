d7693df5cf6d8940281039df080a92439a201ed0
santoshbikkad013 <santoshbikkad013@gmail.com>
2022-11-12 11:38:02 +0530

    SMWDJ-1:complete save operation

---
+++ src/main/java/com/BikkadIT/SpringMvcCrudwithDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCrudwithDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcCrudwithDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/BikkadIT/service/UserServiceI.java
@@ -0,0 +1,8 @@
+package com.BikkadIT.service;
+
+import com.BikkadIT.model.User;
+
+public interface UserServiceI {
+
+	public User saveUser(User user);
+}
